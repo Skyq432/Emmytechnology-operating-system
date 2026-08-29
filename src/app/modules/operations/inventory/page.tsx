6039cdcb56a87d11d7f5d8108480aaf979a80dfb
Skyq432 <Skyq432@gmail.com>
2026-08-29 15:04:02 +0100

    feat: show inventory as of reporting period

--- src/app/modules/operations/inventory/page.tsx
+++ src/app/modules/operations/inventory/page.tsx
@@ -1,9 +1,14 @@
 import { InventoryClient } from '@/components/operations/inventory/inventory-client';
-import { getOperationsInventory, getOperationsLocations } from '@/lib/operations/server';
+import { getOperationsLocations } from '@/lib/operations/server';
+import {
+  getOperationsInventoryForRange,
+  getOperationsReportingRange,
+} from '@/lib/operations/reporting-server';
 
 export default async function OperationsInventoryPage() {
+  const range = await getOperationsReportingRange();
   const [items, locations] = await Promise.all([
-    getOperationsInventory(),
+    getOperationsInventoryForRange(range),
     getOperationsLocations(),
   ]);
   return <InventoryClient items={items} locations={locations} />;
